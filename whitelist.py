4ad30b74d519a3d665b64b64770ae22e5630aee1
Jay Patel <75425454+jayvora18@users.noreply.github.com>
2026-05-06 00:59:14 +0530

    Update whitelist.py

--- whitelist.py
+++ whitelist.py
@@ -1,10 +1,7 @@
-# Whitelisted Chat IDs
 # Users in this list are allowed to register and use the bot
 
 WHITELISTED_CHAT_IDS = [
-    6032060494,  # Admin (jayvora18)
-    8615739734,  # Jasmat
-    # Add more chat IDs below:
-    # 123456789,  # User Name
-    # 987654321,  # Another User
+    123456789,  # Jay
+  # 123456789,  # User Name
+  # 987654321,  # Another User
 ]
