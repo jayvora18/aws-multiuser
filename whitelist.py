24f019c335920a136f6ea9f6539afe1fec7e5223
Jay Patel <75425454+jayvora18@users.noreply.github.com>
2026-05-06 00:57:55 +0530

    Add files via upload

---
+++ whitelist.py
@@ -0,0 +1,10 @@
+# Whitelisted Chat IDs
+# Users in this list are allowed to register and use the bot
+
+WHITELISTED_CHAT_IDS = [
+    6032060494,  # Admin (jayvora18)
+    8615739734,  # Jasmat
+    # Add more chat IDs below:
+    # 123456789,  # User Name
+    # 987654321,  # Another User
+]
